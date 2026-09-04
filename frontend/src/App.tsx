39a5e8e0e2c31d06aae5d3059034af6eb4c45754
ArthurAlencarDeOliveiraSa <arthuraosa@gmail.com>
2026-09-04 11:21:31 -0300

    Limpa o App.tsx para renderizar apenas o Dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,10 @@
-import React, { useState, useEffect } from "react";React
-import Login from "./components/Login";
+import React from "react";
 import Dashboard from "./Dashboard";
 
 export default function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-
-  useEffect(() => {
-    const token = localStorage.getItem("@GSF:token");
-    if (token) {
-      setIsAuthenticated(true);
-    }
-  }, []);
-
-  const handleLoginSuccess = () => {
-    setIsAuthenticated(true);
-  };
-
   return (
     <div>
-      {isAuthenticated ? (
-        <Dashboard />
-      ) : (
-        <Login onLoginSuccess={handleLoginSuccess} />
-      )}
+      <Dashboard />
     </div>
   );
 }
