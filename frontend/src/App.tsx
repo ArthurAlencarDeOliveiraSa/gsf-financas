85e363803c805635e99826917e9c026758a924be
ArthurAlencarDeOliveiraSa <arthuraosa@gmail.com>
2026-09-04 11:11:20 -0300

    Garante rotas /register e /login atualizadas no backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";React
 import Login from "./components/Login";
 import Dashboard from "./Dashboard";
 
